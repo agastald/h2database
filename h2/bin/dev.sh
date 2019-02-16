734f9f66a7d4b24082e792a8c64126adddad7c58
Alessandro <alessandro.gastaldi@gmail.com>
2019-02-16 16:22:24 -0200

    opcao para debug em dev.sh

--- h2/bin/dev.sh
+++ h2/bin/dev.sh
@@ -2,4 +2,7 @@
 dir=$(dirname "$0")
 cd $dir
 find ../temp -name data.zip -exec rm {} \;
-java -cp "../temp:../src/main:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console "$@"
+export JAVA_OPTS="-javaagent:$HOME/lib/jars/simple-proxy-selector-agent.jar -Doracle.net.tns_admin=$HOME/etc"
+export DEBUG_OPTS="-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n"
+[ "-d" = "$1" ] || DEBUG_OPTS=
+java $JAVA_OPTS $DEBUG_OPTS -cp "../temp:../src/main:$H2DRIVERS:$CLASSPATH" org.h2.tools.Console
